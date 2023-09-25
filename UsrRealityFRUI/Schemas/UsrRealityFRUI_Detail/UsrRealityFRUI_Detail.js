a8c3a9f3741f697186b80ddaeab67ccada95386f
Sairam Pashikanti <127sairam@gmail.com>
2023-09-25 12:42:07 +0530

    Reality FreedomUI section added

---
+++ UsrRealityFRUI/Schemas/UsrRealityFRUI_Detail/UsrRealityFRUI_Detail.js
@@ -0,0 +1,8 @@
+define('UsrRealityFRUI_Detail', [], function () {
+	return {
+		entitySchemaName: 'UsrRealityFRUI',
+		details: /**SCHEMA_DETAILS*/ {} /**SCHEMA_DETAILS*/,
+		diff: /**SCHEMA_DIFF*/ [] /**SCHEMA_DIFF*/,
+		methods: {},
+	};
+});
